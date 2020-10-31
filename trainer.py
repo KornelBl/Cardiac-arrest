be36264604965b568da53e35d2d9f2b2acf2ed45
KornelBl <kornelblakala@wp.pl>
2020-10-31 16:51:49 +0100

    Experiment environment is ready

--- trainer.py
+++ trainer.py
@@ -6,30 +6,27 @@ from sklearn.metrics import confusion_matrix
 
 
 def train_evaluate(X, Y, momentum_value, layer_size):
+    print("wszedlem do funkcji")
     rkf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5)
 
-    mlp = MLPClassifier(hidden_layer_sizes=layer_size, max_iter=1000, momentum=momentum_value)
+    mlp = MLPClassifier(hidden_layer_sizes=layer_size, solver="sgd",
+                        max_iter=1000, momentum=momentum_value)
 
     confusion_matrix_sum = np.zeros(shape=(5, 5))
     score_sum = 0
     for train, test in rkf.split(X, Y):
-
         mlp.fit(X[train], Y[train])
 
         predict = mlp.predict(X[test])
 
-        score = mlp.score(X[test], Y[test])
+        temp_score = mlp.score(X[test], Y[test])
         temp_confusion_matrix = confusion_matrix(Y[test], predict)
 
         confusion_matrix_sum += temp_confusion_matrix
-        score_sum += score
+        score_sum += temp_score
+        print(f"Score:{temp_score}\nMatrix:{temp_confusion_matrix}")
 
     score_sum /= 10
     confusion_matrix_sum /= 10
 
     return score_sum, confusion_matrix_sum
-
-
-def save_results_to_csv(scores, confusion_matrix, filename: str = None):
-
-
