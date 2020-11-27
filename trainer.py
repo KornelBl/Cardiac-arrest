cb02f7da3d986cb11f782d36bed77896f509a101
KornelBl <kornelblakala@wp.pl>
2020-11-27 18:51:35 +0100

    fin

--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ from sklearn.metrics import confusion_matrix
 
 def train_evaluate(X, Y, momentum_value, layer_size, train, test):
 
-    mlp = MLPClassifier(hidden_layer_sizes=layer_size, solver="sgd", momentum=momentum_value, max_iter=500, random_state=3)
+    mlp = MLPClassifier(hidden_layer_sizes=layer_size, solver="sgd", momentum=momentum_value, max_iter=1000, random_state=3, batch_size=100)
 
     mlp.fit(X[train], Y[train])
 
